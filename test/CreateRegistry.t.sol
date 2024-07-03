36a8fd82973cecb00f9a929e4ffac35116c34663
Yorke Rhodes <email@yorke.dev>
2024-07-03 18:29:19 -0400

    Update unit tests

--- test/CreateRegistry.t.sol
+++ test/CreateRegistry.t.sol
@@ -12,10 +12,17 @@ contract CreateRegistryTest is Test {
         registry = new CreateRegistry();
     }
 
-    function test_verify() public {
+    function test_verify_hyperlane() public {
         // $ cast to-rlp '["0xa7eccdb9be08178f896c26b7bbd8c3d4e844d9ba","0x00000000000006a9"]'
         bytes memory rlp = hex"d894a7eccdb9be08178f896c26b7bbd8c3d4e844d9ba8206a9";
         address deployed = 0xc005dc82818d67AF737725bD4bf75435d065D239;
         registry.verify(deployed, rlp);
+        assert(registry.deployers(deployed) == 0xa7ECcdb9Be08178f896c26b7BbD8C3D4E844d9Ba);
+    }
+
+    function test_verify(address deployer, address deployed, bytes calldata rlpBytes) public {
+        vm.expectRevert();
+        registry.verify(deployed, rlpBytes);
+        assert(registry.deployers(deployed) != address(deployer));
     }
 }
